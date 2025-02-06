d6f8b8a41848eaff8bb38acae8023fdbb23cf0fd
Matt Buckley <mattbuckley@google.com>
2025-02-06 06:32:09 -0800

    Remove FrameTimeline module and build FrameTimeline.cpp with SF
    FrameTimeline does not need to be a separate module and it makes
    linking against it difficult for no obvious benefit
    
    Bug: 360908317
    Test: atest libsurfaceflinger_unittest
    Flag: EXEMPT refactor
    Change-Id: I0d20a2bf9e93dedfb03b44eeb0356f7024e36559

--- services/surfaceflinger/Scheduler/EventThread.cpp
+++ services/surfaceflinger/Scheduler/EventThread.cpp
@@ -45,7 +45,7 @@
 #include <common/FlagManager.h>
 #include <scheduler/VsyncConfig.h>
 #include "DisplayHardware/DisplayMode.h"
-#include "FrameTimeline.h"
+#include "FrameTimeline/FrameTimeline.h"
 #include "VSyncDispatch.h"
 #include "VSyncTracker.h"
 
