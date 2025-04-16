c661b06ee70995b8dd269c7750887745ca7943c7
Bo Zhang <bo.zhang@nxp.com>
2025-04-16 14:08:51 +0900

    Update expected present time to optimize refresh rate
    When the GPU/HWC composition cost too much time and complete in next VSYNC period,
    the next frame will be skipped. But actually the SurfaceFlinger commit/present a
    frame can be paralleled with previous frame composition(GPU3D or HWC). So we can
    suppose that this frame may be present in 2 or more Vsync period.
    
    Adding one more vsync period to expected present time for this frame if previous
    frame does not present(present fence is not signaled). So this frame can prepare
    early.
    The expected present time that send to HWC still keep to use the original expected
    present time to avoid HWC wait too loog time when present frame.
    
    Change-Id: I3345234e3e9a257d75e4d6096be967711147e624
    Signed-off-by: Bo Zhang <bo.zhang@nxp.com>

--- services/surfaceflinger/Scheduler/include/scheduler/FrameTargeter.h
+++ services/surfaceflinger/Scheduler/include/scheduler/FrameTargeter.h
@@ -52,6 +52,7 @@ public:
     Duration expectedFrameDuration() const { return mExpectedPresentTime - mFrameBeginTime; }
 
     TimePoint expectedPresentTime() const { return mExpectedPresentTime; }
+    TimePoint scheduledPresentTime() const { return mScheduledPresentTime; }
 
     std::optional<TimePoint> earliestPresentTime() const { return mEarliestPresentTime; }
 
@@ -69,6 +70,7 @@ protected:
     ~FrameTarget() = default;
 
     bool wouldPresentEarly(Period vsyncPeriod, Period minFramePeriod) const;
+    void UpdateExpectedPresentTime(TimePoint& expectedPresentTime, Period vsyncPeriod) const;
 
     // Equivalent to `pastVsyncTime` unless running N VSYNCs ahead.
     TimePoint previousFrameVsyncTime(Period minFramePeriod) const {
@@ -83,6 +85,7 @@ protected:
     VsyncId mVsyncId;
     TimePoint mFrameBeginTime;
     TimePoint mExpectedPresentTime;
+    TimePoint mScheduledPresentTime;
     std::optional<TimePoint> mEarliestPresentTime;
 
     TracedOrdinal<bool> mFramePending;
@@ -164,7 +167,6 @@ private:
     const bool mSupportsExpectedPresentTime;
     const bool mPropagateBackpressure;
 
-    TimePoint mScheduledPresentTime;
     CompositionCoverageFlags mCompositionCoverage;
 
     std::atomic_uint mFrameMissedCount = 0;
